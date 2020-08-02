647be9bc2112e59442c9d99681e4ea76ed83292d
郑康 <wx_9fd5880b174e4fdb84a2db25d97afe3a@git.code.tencent.com>
2020-08-02 16:30:58 +0800

    solve p19

---
+++ 19.cpp
@@ -0,0 +1,22 @@
+/*
+    https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+*/
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *next = node->next;
+        node->val = next->val;
+        node->next = next->next;
+        next->next = nullptr;
+        delete next;
+        next = nullptr;
+    }
+};
