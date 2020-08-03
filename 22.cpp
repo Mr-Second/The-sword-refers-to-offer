41229e2f601bfcb03bbd0616ca254b696cd8e228
郑康 <wx_9fd5880b174e4fdb84a2db25d97afe3a@git.code.tencent.com>
2020-08-03 11:22:38 +0800

    solve p22

--- 22.cpp
+++ 22.cpp
@@ -1,16 +1,41 @@
 /*
     https://www.nowcoder.com/practice/6f8c901d091949a5837e24bb82a731f2?tpId=13&&tqId=11206&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 */
-#include <regex>
+
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class Solution {
+    int index = 0;
 public:
     bool isNumeric(char* str)
     {
-        regex pattern("[\\+\\-]?\\d*(\\.\\d+)?([eE][\\+\\-]?\\d+)?");
-        return regex_match(str, pattern);
+        if(!str)
+            return false;
+        bool numberic = scanInteger(str);
+        if(index < strlen(str) && str[index] == '.') {
+            ++index;
+            numberic = scanUnsignedInteger(str) || numberic;
+        }
+        if(index < strlen(str) && str[index] == 'e' || str[index] == 'E') {
+            ++index;
+            numberic = numberic && scanInteger(str);
+        }
+        return numberic && index == strlen(str);
+    }
+
+    bool scanUnsignedInteger(const char* str) 
+    {
+        int start = index;
+        while(index < strlen(str) && str[index] >= '0' && str[index] <= '9') index++;
+        return start < index;
+
+    }
+
+    bool scanInteger(const char* str) {
+        if(index < strlen(str) && (str[index] == '+' || str[index] == '-')) index++;\
+        return scanUnsignedInteger(str);
     }
 };
 
