e3392060e70e7e03a4fd5cb5744d48f75d164155
郑康 <wx_9fd5880b174e4fdb84a2db25d97afe3a@git.code.tencent.com>
2020-08-13 16:17:58 +0800

    solve p60

---
+++ 60.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if(!pRoot)
+            return 0;
+        return max(TreeDepth(pRoot->left), TreeDepth(pRoot->right)) + 1;
+    }
+};
